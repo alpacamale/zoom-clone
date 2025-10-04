23a29c4cd13139ac343e54a70b68877c31998a59
alpacamale <alpaca9100@gmail.com>
2025-10-04 22:14:49 +0900

    feat: simple websocket communication

--- src/server.js
+++ src/server.js
@@ -1,9 +1,27 @@
+import http from "http";
+import WebSocket from "ws";
 import express from "express";
+
+const port = 3000;
 
 const app = express();
 app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (req, res) => res.render("home"));
-const handleListen = () => console.log("Listening on http://localhost:3000");
-app.listen(3000, handleListen);
+app.use((req, res) => res.redirect("/"));
+
+const handleListen = () => console.log(`Listening on http://localhost:${port}`);
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+wss.on("connection", (socket) => {
+  console.log("Connected to Browser ✅");
+  socket.on("close", () => console.log("Disconnected from Browser ❌"));
+  socket.on("message", (message) => {
+    console.log(message.toString("utf-8"));
+  });
+  socket.send("hello!!");
+});
+
+server.listen(port, handleListen);
